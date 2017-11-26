bc12d20ffadbee032a5ae7fff9254f868372ea84
Adam Gall <adam@decentcrypto.com>
2017-11-26 00:44:40 -0500

    Add test file

---
+++ test/TestToken.js
@@ -0,0 +1,78 @@
+var ATXCrowdsale = artifacts.require("./contracts/ATXCrowdsale.sol");
+var StandardToken = artifacts.require("./contracts/zeppelin/token/StandardToken.sol");
+
+function atxToWei(value){
+    return value * Math.pow(10, 18);
+}
+
+contract('ATXCrowdsale', function(accounts) {
+    it("Create and send tokens", function(){
+        currentTime = Math.floor(Date.now() / 1000);
+        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
+            this.instanceAtx = instance;
+            this.initialBalanceFunding = web3.eth.getBalance(accounts[0]).toNumber();
+            return this.instanceAtx.createTokens({from: accounts[1], value: web3.toWei('1', 'ether')});
+        }).then(function(){
+            return instanceAtx.token();
+        }).then(function(token){
+            return StandardToken.at(token);
+        }).then(function(tokenInstance){
+            this.token = tokenInstance;
+            return token.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), atxToWei(4000), "Account 1 should have 4000 ATX");
+            return token.transfer(accounts[2], atxToWei(1000), { from: accounts[1] });
+        }).then(function(){
+            return token.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), atxToWei(3000), "Account 1 should have 1000 ATX less");
+            return token.balanceOf(accounts[2]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), atxToWei(1000), "Account 2 should have 1000 ATX");
+            diff = web3.eth.getBalance(accounts[0]).toNumber() - this.initialBalanceFunding;
+            assert.isAbove(diff, atxToWei(1 - 0.01), "Eth fund address should have 1 ETH more")
+        });
+    });
+    it("Should not buy tokens, min amount limit", function(){
+        currentTime = Math.floor(Date.now() / 1000);
+        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
+            this.instanceAtx = instance;
+            return this.instanceAtx.setWhitelist(accounts[1], atxToWei(20 * 4000), { from: accounts[0] })
+        }).then(function(){
+            return instanceAtx.token();
+        }).then(function(token){
+            return StandardToken.at(token);
+        }).then(function(tokenInstance){
+            this.token2 = tokenInstance;
+            this.instanceAtx = instance;
+            return instanceAtx.createTokens({from: accounts[1], value: web3.toWei('0.09', 'ether')});
+        }).catch(function(exception){
+            this.savedException = exception;
+        }).then(function(){
+            assert.isNotNull(savedException, "Sould have throw an exception")
+            return token2.balanceOf(accounts[2]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 0, "Account 1 should have 0 ATX");
+        });
+    });
+    it("Should not buy tokens, whitelist", function(){
+        currentTime = Math.floor(Date.now() / 1000);
+        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
+            this.instanceAtx = instance;
+            return instanceAtx.token();
+        }).then(function(token){
+            return StandardToken.at(token);
+        }).then(function(tokenInstance){
+            this.token2 = tokenInstance;
+            this.instanceAtx = instance;
+            return instanceAtx.createTokens({from: accounts[1], value: web3.toWei('2', 'ether')});
+        }).catch(function(exception){
+            this.savedException = exception;
+        }).then(function(){
+            assert.isNotNull(savedException, "Sould have throw an exception")
+            return token2.balanceOf(accounts[2]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 0, "Account 1 should have 0 ATX");
+        });
+    });
+});
