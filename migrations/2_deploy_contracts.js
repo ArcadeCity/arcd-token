70a4f9755d4c4f2a076c166f20bacf385a864a45
Adam Gall <adam@decentcrypto.com>
2017-11-27 15:22:11 -0500

    Actually don't deploy an independent instance of ArcadeToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var ATXCrowdsale = artifacts.require("./ATXCrowdsale.sol");
-var ATXToken = artifacts.require("./ATXToken.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(ATXToken);
   deployer.deploy(ATXCrowdsale);
 };
