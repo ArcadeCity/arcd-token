940e6164d1fb75c96049721c0e6fc911ac0244c5
Adam Gall <adam@decentcrypto.com>
2017-11-27 01:07:10 -0500

    Hardcode in a gas price to the truffle config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      gasPrice: 60000000000,
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
