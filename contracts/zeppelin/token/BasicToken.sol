aa1531dbb291686acdcef772c56dfcf88b87df4f
Adam Gall <adam@decentcrypto.com>
2017-11-27 01:06:53 -0500

    Delete commented code

--- contracts/zeppelin/token/BasicToken.sol
+++ contracts/zeppelin/token/BasicToken.sol
@@ -19,15 +19,6 @@ contract BasicToken is ERC20Basic {
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  /*function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-
-    // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }*/
   function transfer(address _to, uint256 _value) public returns (bool success) {
     if (balances[msg.sender] >= _value) {
       balances[msg.sender] = balances[msg.sender].sub(_value);
