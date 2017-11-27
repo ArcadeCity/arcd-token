26af1a13ae271e04ff45b14daada76b56b4c2f99
Adam Gall <adam@decentcrypto.com>
2017-11-27 01:12:26 -0500

    Fix tests

--- test/TestToken.js
+++ test/TestToken.js
@@ -1,78 +1,82 @@
 var ATXCrowdsale = artifacts.require("./contracts/ATXCrowdsale.sol");
-var StandardToken = artifacts.require("./contracts/zeppelin/token/StandardToken.sol");
+var ATXToken = artifacts.require("./contracts/ATXToken.sol");
 
 function atxToWei(value){
-    return value * Math.pow(10, 18);
+    return value * (10**18);
 }
 
 contract('ATXCrowdsale', function(accounts) {
-    it("Create and send tokens", function(){
-        currentTime = Math.floor(Date.now() / 1000);
-        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
-            this.instanceAtx = instance;
-            this.initialBalanceFunding = web3.eth.getBalance(accounts[0]).toNumber();
-            return this.instanceAtx.createTokens({from: accounts[1], value: web3.toWei('1', 'ether')});
-        }).then(function(){
-            return instanceAtx.token();
-        }).then(function(token){
-            return StandardToken.at(token);
-        }).then(function(tokenInstance){
-            this.token = tokenInstance;
-            return token.balanceOf(accounts[1]);
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), atxToWei(4000), "Account 1 should have 4000 ATX");
-            return token.transfer(accounts[2], atxToWei(1000), { from: accounts[1] });
-        }).then(function(){
-            return token.balanceOf(accounts[1]);
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), atxToWei(3000), "Account 1 should have 1000 ATX less");
-            return token.balanceOf(accounts[2]);
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), atxToWei(1000), "Account 2 should have 1000 ATX");
-            diff = web3.eth.getBalance(accounts[0]).toNumber() - this.initialBalanceFunding;
-            assert.isAbove(diff, atxToWei(1 - 0.01), "Eth fund address should have 1 ETH more")
-        });
+  let atxCrowdsale;
+  let atxToken;
+
+  let ethFundAddress;
+  let atxDepositAddress;
+  let saleStartTimestamp;
+  let saleEndTimesamp;
+
+  let alice;
+  let bob;
+
+  beforeEach(function() {
+    currentTime = Math.floor(Date.now() / 1000);
+
+    ethFundAddress = accounts[0];
+    atxDepositAddress = accounts[1];
+    saleStartTimestamp = currentTime - 1;
+    saleEndTimesamp = currentTime + 10; // TODO: this might be a problem eventually
+
+    alice = accounts[2];
+    bob = accounts[3];
+
+    return ATXCrowdsale.new(ethFundAddress, atxDepositAddress, saleStartTimestamp, saleEndTimesamp)
+    .then(function(instance) {
+      atxCrowdsale = instance;
+      return atxCrowdsale.token();
+    }).then(function(token) {
+      atxToken = ATXToken.at(token);
     });
-    it("Should not buy tokens, min amount limit", function(){
-        currentTime = Math.floor(Date.now() / 1000);
-        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
-            this.instanceAtx = instance;
-            return this.instanceAtx.setWhitelist(accounts[1], atxToWei(20 * 4000), { from: accounts[0] })
-        }).then(function(){
-            return instanceAtx.token();
-        }).then(function(token){
-            return StandardToken.at(token);
-        }).then(function(tokenInstance){
-            this.token2 = tokenInstance;
-            this.instanceAtx = instance;
-            return instanceAtx.createTokens({from: accounts[1], value: web3.toWei('0.09', 'ether')});
-        }).catch(function(exception){
-            this.savedException = exception;
-        }).then(function(){
-            assert.isNotNull(savedException, "Sould have throw an exception")
-            return token2.balanceOf(accounts[2]);
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), 0, "Account 1 should have 0 ATX");
-        });
+  });
+
+  it("should seed initial address with tokens", function() {
+    let atxFund = 92 * (10**8) * (10**18);
+
+    return atxToken.balanceOf(atxDepositAddress)
+    .then(function(balance) {
+      assert.equal(balance.toNumber(), atxFund, "atx seed fund account should have 9.2B ATX");
     });
-    it("Should not buy tokens, whitelist", function(){
-        currentTime = Math.floor(Date.now() / 1000);
-        return ATXCrowdsale.new(accounts[0], accounts[0], currentTime - 1, currentTime + 10).then(function(instance){
-            this.instanceAtx = instance;
-            return instanceAtx.token();
-        }).then(function(token){
-            return StandardToken.at(token);
-        }).then(function(tokenInstance){
-            this.token2 = tokenInstance;
-            this.instanceAtx = instance;
-            return instanceAtx.createTokens({from: accounts[1], value: web3.toWei('2', 'ether')});
-        }).catch(function(exception){
-            this.savedException = exception;
-        }).then(function(){
-            assert.isNotNull(savedException, "Sould have throw an exception")
-            return token2.balanceOf(accounts[2]);
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), 0, "Account 1 should have 0 ATX");
-        });
+  });
+
+  it("should allow tokens to be created and sent", function() {
+    let initialBalanceFunding = web3.eth.getBalance(ethFundAddress).toNumber();
+
+    return atxCrowdsale.createTokens({ from: alice, value: web3.toWei('1', 'ether') })
+    .then(function() {
+      return atxToken.balanceOf(alice);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), atxToWei(200000), "alice should have 200000 ATX");
+      return atxToken.transfer(bob, atxToWei(50000), { from: alice });
+    }).then(function() {
+      return atxToken.balanceOf(alice);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), atxToWei(150000), "alice should have 50000 ATX less");
+      return atxToken.balanceOf(bob);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), atxToWei(50000), "bob should have 50000 ATX");
+
+      diff = web3.eth.getBalance(ethFundAddress).toNumber() - initialBalanceFunding;
+      assert.isAbove(diff, atxToWei(1 - 0.01), "Eth fund address should have 1 ETH more")
     });
+  });
+
+  it("Should not buy tokens, min amount limit", function(){
+    return atxCrowdsale.createTokens({ from: alice, value: web3.toWei('0.09', 'ether') })
+    .catch(function(exception){
+        this.savedException = exception;
+    }).then(function() {
+        assert.isNotNull(savedException, "Should have throw an exception")
+        return atxToken.balanceOf(alice);
+    }).then(function(balance){
+        assert.equal(balance.toNumber(), 0, "alice should have 0 ATX");
+    });
+  });
 });
